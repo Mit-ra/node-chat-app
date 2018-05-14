fc16e78e9eea0b6e0988eed1e81d96934c711aa9
mit-ra <mitra912@gmail.com>
2018-05-14 11:00:36 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,6 @@ socket.on('newMessage', function (message) {
   li.text(`${message.from}: ${message.text}`);
 
   jQuery('#messages').append(li);
-
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -24,36 +23,38 @@ socket.on('newLocationMessage', function (message) {
   li.text(`${message.from}: `);
   a.attr('href', message.url);
   li.append(a);
-
   jQuery('#messages').append(li);
-
 });
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('')
   });
 });
 
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function() {
+locationButton.on('click', function () {
   if (!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser');
+    return alert('Geolocation not supported by your browser.');
   }
 
-  navigator.geolocation.getCurrentPosition(function(position) {
-      socket.emit('createLocationMessage', {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      })
-  }, function() {
-    alert('Unable to fetch location');
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
   });
-
-
 });
